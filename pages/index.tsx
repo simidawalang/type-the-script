10727f50c0b8a2b42552e10135bacdc219d82779
simidawalang <simidawalang@gmail.com>
2022-07-24 07:14:01 +0100

    styled home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,22 +8,35 @@ const Home: NextPage = () => {
       <Head>
         <title>Type the Script</title>
         <meta name="description" content="A typing app" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <header className="home-header">
-        <div className="intro">
-          <h1>
-            Type <span>the</span> Script
+        <div className="intro container bg-brand text-brand-2">
+          <h1 className="app-name">
+            <span>Type</span>
+            <span className="text-sm">the</span>
+            <span>Script</span>
           </h1>
         </div>
-        <div className="intro">
-          <h2>Hi there...</h2>
-          <p>
-            Need a simple app to improve your typing
-            skills? Type the Script is perfect for you.
-          </p>
-          <a href="/play">Get started</a>
+        <div className="intro container bg-gray app-description">
+          <div>
+            <h2 className="welcome-text">Hi there...</h2>
+            <p>
+              Need a simple app to improve your typing skills? Type the Script
+              is perfect for you. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Rem enim adipisci, provident iusto cumque natus
+              quisquam nisi? Corrupti maiores repudiandae consectetur enim
+              recusandae cum atque delectus fugiat, dolore magni voluptatum?
+            </p>
+            <a className="btn btn-brand get-started-link" href="/play">
+              Get started
+            </a>
+          </div>
         </div>
       </header>
     </div>
